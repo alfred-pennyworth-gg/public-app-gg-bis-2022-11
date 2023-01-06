9d667216750044709be77af7ee8c759ff028c041
Alfred Pennyworth <alfred-pennyworth-gg@protonmail.com>
2023-01-06 09:33:25 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 
 import antigravity
 
-client_id=3MVG9Cjji0HXqDr0h1nvNCXOQ7SHbdqd.S5TXvb.4A97JUDmad4ITs4lW4W2DuUWIPWW5sDtarjpR30Uw9Zjj
-client_secret=96796196FB138E1A48E862390A87D2B60BE246D5473B5B399B86C88402F67270
+SEED=b5870fbd-9a92-4447-ad76-b27be23a8096
 
 def main():
     antigravity.fly()
